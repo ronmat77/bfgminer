dcdc50d52fe2c26e571d830f295ebd9208569a41
Luke Dashjr <luke-jr+git@utopios.org>
2014-02-16 07:06:59 +0000

    opencl: Change xintensity interface to simply intensity=xNNN (and allow setting d<whatever> for an initial dynamic intensity)

--- driver-opencl.c
+++ driver-opencl.c
@@ -22,6 +22,7 @@
 #include <windows.h>
 #endif
 
+#include <ctype.h>
 #include <math.h>
 #include <string.h>
 #include <stdbool.h>
@@ -551,19 +552,44 @@ unsigned long intensity_to_oclthreads(double intensity, const bool is_sha256d)
 	return pow(2, intensity);
 }
 
+unsigned long xintensity_to_oclthreads(const double xintensity, const cl_uint max_compute_units)
+{
+	return xintensity * max_compute_units * 0x40;
+}
+
 static
-bool _set_intensity(struct cgpu_info * const cgpu, const char * const _val)
+bool _set_intensity(struct cgpu_info * const cgpu, const char *_val)
 {
 	struct opencl_device_data * const data = cgpu->device_data;
+	
+	data->dynamic = false;
 	data->_init_xintensity = 0;
+	
 	if (!strncasecmp(_val, "d", 1))
+	{
 		data->dynamic = true;
+		++_val;
+	}
+	
+	if (!strncasecmp(_val, "x", 1))
+	{
+		const double v = atof(&_val[1]);
+		if (v < 1 || v > 9999)
+			return false;
+		
+		struct thr_info * const thr = cgpu->thr[0];
+		const int thr_id = thr->id;
+		_clState * const clState = clStates[thr_id];
+		
+		data->oclthreads = xintensity_to_oclthreads(v, clState->max_compute_units);
+		data->_init_xintensity = v;
+	}
 	else
+	if (isdigit(_val[0]))
 	{
 		const double v = atof(_val);
 		if (v < MIN_INTENSITY || v > MAX_GPU_INTENSITY)
 			return false;
-		data->dynamic = false;
 		data->oclthreads = intensity_to_oclthreads(v, !opt_scrypt);
 	}
 	pause_dynamic_threads(cgpu->device_id);
@@ -574,36 +600,6 @@ const char *set_intensity(char *arg)
 {
 	return _set_list(arg, "Invalid value passed to intensity", _set_intensity);
 }
-
-unsigned long xintensity_to_oclthreads(const double xintensity, const cl_uint max_compute_units)
-{
-	return xintensity * max_compute_units * 0x40;
-}
-
-static
-bool _set_xintensity(struct cgpu_info * const cgpu, const char * const _val)
-{
-	struct opencl_device_data * const data = cgpu->device_data;
-	if (!strncasecmp(_val, "d", 1))
-		data->dynamic = true;
-	else
-	{
-		const double v = atof(_val);
-		if (v < 1 || v > 9999)
-			return false;
-		data->dynamic = false;
-		
-		struct thr_info * const thr = cgpu->thr[0];
-		const int thr_id = thr->id;
-		_clState * const clState = clStates[thr_id];
-		
-		data->oclthreads = xintensity_to_oclthreads(v, clState->max_compute_units);
-		data->_init_xintensity = v;
-	}
-	pause_dynamic_threads(cgpu->device_id);
-	return true;
-}
-_SET_INTERFACE(xintensity)
 
 _SET_INT_LIST2(gpu_threads, (v >= 1 && v <= 10), cgpu->threads)
 #endif
@@ -1789,7 +1785,6 @@ const char *opencl_cannot_set(struct cgpu_info * const proc, const char * const 
 
 static const struct bfg_set_device_definition opencl_set_device_funcs_probe[] = {
 	{"intensity", opencl_init_intensity},
-	{"xintensity", opencl_init_xintensity},
 	{"kernel", opencl_init_kernel},
 	{"threads", opencl_init_gpu_threads},
 	{"vector", opencl_init_vector},
@@ -1813,8 +1808,7 @@ static const struct bfg_set_device_definition opencl_set_device_funcs_probe[] = 
 };
 
 static const struct bfg_set_device_definition opencl_set_device_funcs[] = {
-	{"intensity", opencl_init_intensity, "Intensity of GPU scanning (d or -10 -> 31)"},
-	{"xintensity", opencl_init_xintensity, "Intensity of GPU scanning (shader-based; 1 to 9999)"},
+	{"intensity", opencl_init_intensity, "Intensity of GPU scanning (d, -10 -> 31, or x1 to x9999)"},
 	{"kernel", opencl_cannot_set, "Mining kernel code to use"},
 	{"threads", opencl_cannot_set, "Number of threads"},
 	{"vector", opencl_cannot_set, ""},
